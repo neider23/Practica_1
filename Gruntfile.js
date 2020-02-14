39e26481049b5f820c5bb2e04b725344b922842f
NeiderCa <neidercarrillo@gmail.com>
2020-02-14 07:34:29 -0500

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,8 +8,8 @@ module.exports = function(grunt) {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        src: 'Js/Index.js',
+        dest: 'Js/Index.min.js'
       }
     }
   });
